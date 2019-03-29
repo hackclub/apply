8a865259a703b2b643f2e1d3bded83ac68e2c7ea
Lachlan Campbell <lachlan@hackclub.com>
2019-03-28 17:16:38 -0700

    Reload on logout

--- src/components/auth/LogoutButton.js
+++ src/components/auth/LogoutButton.js
@@ -6,6 +6,7 @@ import storage from 'storage'
 export const destroySession = () => {
   storage.remove('authToken')
   storage.remove('userId')
+  window.reload()
 }
 
 Button.link = Button.withComponent(Link)
