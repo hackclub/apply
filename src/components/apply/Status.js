bf93f0b5d482b18c776ce3e6ea96fee2a8b4bfc2
Max Wofford <max@maxwofford.com>
2019-08-03 18:16:58 -0700

    status badges for co-leader profiles look less actionable

--- src/components/apply/Status.js
+++ src/components/apply/Status.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Badge } from '@hackclub/design-system'
 
-const Status = ({ type, ...props }) => {
+const Status = ({ type, actionable=true, ...props }) => {
   const data = {
     unopened: { bg: 'primary', children: 'not started' },
     incomplete: { bg: 'warning', children: 'in progress' },
     complete: { bg: 'success', children: 'complete' },
     submitted: { bg: 'success', children: 'submitted' }
   }[type]
-  return <Badge normal {...data} {...props} />
+  const style = {
+    opacity: actionable ? 1 : 0.6
+  }
+  return <Badge normal {...data} style={style} {...props}/>
 }
 
 export default Status
