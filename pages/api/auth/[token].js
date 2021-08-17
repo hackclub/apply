5fca32d919557bb23185bdc1088f5697a8a49881
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-08-17 20:56:18 +0800

    Fix Locale Sign Ins

--- pages/api/auth/[token].js
+++ pages/api/auth/[token].js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
         path: '/',
       })
     }
-    res.redirect(`/${tokenRecord.fields['Path'] ? tokenRecord.fields['Path'] : ''}`)
+    res.redirect(`/${tokenRecord.fields['Locale with a slash'] ? tokenRecord.fields['Locale with a slash'] : ''}${tokenRecord.fields['Path'] ? tokenRecord.fields['Path'] : ''}`)
   } catch {
     res.redirect('/')
   }
