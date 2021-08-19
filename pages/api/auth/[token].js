49ffd65b79fd6ca6a9b9533a94ef86ceb357c245
leomcelroy <leomcelroy@gmail.com>
2021-08-19 13:41:31 -0400

    Update [token].js

--- pages/api/auth/[token].js
+++ pages/api/auth/[token].js
@@ -1,9 +1,9 @@
-import { loginsAirtable } from '../../../lib/airtable'
+import { base } from '/lib/base'
 import nookies from 'nookies'
 
 export default async function handler(req, res) {
   try {
-    const tokenRecord = await loginsAirtable.find('rec' + req.query.token)
+    const tokenRecord = await base("Logins").find('rec' + req.query.token)
     if (tokenRecord.fields['Path']) {
       nookies.set({ res }, 'authToken', req.query.token, {
         maxAge: 30 * 24 * 60 * 60,
