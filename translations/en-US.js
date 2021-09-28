d6d5ef2fdf536df28ba9db79389a80cca054a0b7
Max Wofford <max@maxwofford.com>
2021-09-28 09:59:49 -0400

    Contact email "applications@" -> "clubs@"

--- translations/en-US.js
+++ translations/en-US.js
@@ -1,5 +1,5 @@
 export const messageObject =  {
-  "CONTACT_EMAIL": "applications@hackclub.com",
+  "CONTACT_EMAIL": "clubs@hackclub.com",
   "CONTACT_MESSAGE": "Please don’t hesitate to reach out. We’re available to email at",
   "APPLICATION_STATUS_MESSAGE": "Your application to start a Hack Club",
   "APPLICATION_STATUS_MESSAGE_APPENDED_IS": "is",
