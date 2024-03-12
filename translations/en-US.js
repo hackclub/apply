5be7e0df5c43205588896398ff3f788cad6232ae
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2024-03-12 18:15:06 -0400

    Update en-US.js

--- translations/en-US.js
+++ translations/en-US.js
@@ -94,7 +94,7 @@ export const messageObject = {
   EXCITED_TO_HAVE_YOU: "We're excited to have you in the Hack Club community!",
   SCHEDULED_ONBOARDING: "We've scheduled your onboarding with",
   CHECK_YOUR_EMAIL_ONBOARDING:
-    'Check your email for more information. Please come prepared to talk about your club and your goals for the following year!',
+    'Check your email for more information. Look forward to meeting you!',
   COMPLETE_MESSAGE:
     "Congratulations on completing your club onboarding! Now, the club is what you make of it. You'll have a check-in call in one month from your onboarding; please check your email for more information. We're excited to get your club up and running!",
   PROUDLY_OPEN_SOURCE: 'Proudly Open Source',
